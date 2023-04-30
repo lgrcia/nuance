6f6b11cf2c54aaa80f8a3c99fa805abf6d279b60
lgrcia <lionel_garcia@live.fr>
2023-04-30 22:25:10 +0200

    feat!: pmap linear search

--- nuance/nuance.py
+++ nuance/nuance.py
@@ -1,33 +1,45 @@
-import jax
-from tinygp import kernels, GaussianProcess
-import numpy as np
-import jax.numpy as jnp
-from . import utils
-from tqdm.autonotebook import tqdm
-from .search_data import SearchData
-import jaxopt
-import numpy as np
-from dataclasses import dataclass
 import multiprocessing as mp
-from functools import partial
-from multiprocessing import cpu_count
-from . import CPU_counts
+from dataclasses import dataclass
+from multiprocessing import set_start_method
+
+import jax
+import jax.numpy as jnp
+import jaxopt
 import multiprocess as mp
-from tqdm import tqdm
-from dataclasses import asdict
-from copy import deepcopy
+import numpy as np
+from tinygp import GaussianProcess, kernels
+from tqdm.autonotebook import tqdm
+
+from . import CPU_counts, utils
+from .search_data import SearchData
+
+set_start_method("spawn")
+
 import pickle
+from copy import deepcopy
+from dataclasses import asdict
+
+from tqdm import tqdm
+
 
 @dataclass
 class Nuance:
     time: np.ndarray
+    """Time"""
     flux: np.ndarray
+    """Flux time series"""
     error: np.ndarray = None
-    kernel: None = None
+    """Flux error time series"""
+    gp: GaussianProcess = None
+    """Gaussian process instance"""
     X: np.ndarray = None
+    """Design matrix"""
     compute: bool = True
+    """Whether to pre-compute the Cholesky decomposition"""
     mean: float = 0.0
+    """Mean of the GP"""
     search_data: SearchData = None
+    """Search data instance"""
 
     def __post_init__(self):
         """Nuance
@@ -43,13 +55,19 @@ class Nuance:
         X : ndarray, optional
             design matrix, by default None
         """
+        assert (self.error is None) ^ (
+            self.gp is None
+        ), "Either error or gp must be defined"
+
         if self.X is None:
             self.X = np.atleast_2d(np.ones_like(self.time))
 
-        if self.kernel is None:
-            self.kernel = kernels.Constant(0.0)
+        if self.gp is None:
+            kernel = kernels.quasisep.Exp(1e12)
 
-        self.gp = GaussianProcess(self.kernel, self.time, diag=self.error**2, mean=self.mean)
+            self.gp = GaussianProcess(
+                kernel, self.time, diag=self.error**2, mean=self.mean
+            )
 
         if self.compute:
             self._compute_L()
@@ -57,7 +75,6 @@ class Nuance:
         self.search_data = None
 
     def _compute_L(self):
-
         Liy = self.gp.solver.solve_triangular(self.flux)
         LiX = self.gp.solver.solve_triangular(self.X.T)
 
@@ -110,16 +127,16 @@ class Nuance:
         """
 
         n = len(self.X)
-        
+
         @jax.jit
         def eval_transit(t0, D):
             m = utils.single_transit(self.time, t0, D)
             _ll, w, v = self.eval_m(m)
             return w[n], v[n, n], _ll
 
-        chunk_size = CPU_counts 
-        chunks = int(np.ceil(len(t0s)/chunk_size))
-        padded_t0s = np.pad(t0s, pad_width=[0, chunks*chunk_size-len(t0s)])
+        chunk_size = CPU_counts
+        chunks = int(np.ceil(len(t0s) / chunk_size))
+        padded_t0s = np.pad(t0s, pad_width=[0, chunks * chunk_size - len(t0s)])
         splitted_t0s = np.array(np.array_split(padded_t0s, chunks))
 
         ll = np.zeros((len(padded_t0s), len(Ds)))
@@ -127,17 +144,19 @@ class Nuance:
         vars = ll.copy()
         depths = ll.copy()
 
+        _progress = lambda x: tqdm(x, unit_scale=CPU_counts) if progress else x
+
         f = jax.pmap(eval_transit, in_axes=(0, None))
         g = jax.vmap(f, in_axes=(None, 0))
-        for i, t0 in enumerate(tqdm(splitted_t0s, unit_scale=CPU_counts)):
+        for i, t0 in enumerate(_progress(splitted_t0s)):
             _depths, _vars, _ll = g(t0, Ds)
-            depths[i*CPU_counts:(i+1)*CPU_counts, :] = _depths.T
-            vars[i*CPU_counts:(i+1)*CPU_counts, :] = _vars.T
-            ll[i*CPU_counts:(i+1)*CPU_counts, :] = _ll.T
+            depths[i * CPU_counts : (i + 1) * CPU_counts, :] = _depths.T
+            vars[i * CPU_counts : (i + 1) * CPU_counts, :] = _vars.T
+            ll[i * CPU_counts : (i + 1) * CPU_counts, :] = _ll.T
 
-        depths = np.array(depths[0:len(t0s), :])
-        vars = np.array(vars[0:len(t0s), :])
-        ll = np.array(ll[0:len(t0s), :])
+        depths = np.array(depths[0 : len(t0s), :])
+        vars = np.array(vars[0 : len(t0s), :])
+        ll = np.array(ll[0 : len(t0s), :])
 
         if positive:
             ll0 = self.eval_m(np.zeros_like(self.time))[0]
@@ -149,7 +168,7 @@ class Nuance:
             t0s=t0s, Ds=Ds, ll=ll, z=depths, vz=vars, ll0=self.ll0
         )
 
-    def periodic_search(self, periods: np.ndarray, progress=True, fancy=True):
+    def periodic_search(self, periods: np.ndarray, progress=True):
         """Performs the periodic search
 
         Parameters
@@ -165,19 +184,20 @@ class Nuance:
             search results
         """
         new_search_data = self.search_data.copy()
-        fold_ll = new_search_data.fold_ll
         n = len(periods)
         snr = np.zeros(n)
-        max_ll = snr.copy()
         params = np.zeros((n, 3))
 
-        def _progress(x, **kwargs): return tqdm(x, **kwargs) if progress else x
-        
+        def _progress(x, **kwargs):
+            return tqdm(x, **kwargs) if progress else x
+
         global SEARCH
         SEARCH = self.search_data.fold_ll
-    
+
         with mp.Pool() as pool:
-            for p, (Ti, j, P) in enumerate(_progress(pool.imap(_search, periods), total=len(periods))):
+            for p, (Ti, j, P) in enumerate(
+                _progress(pool.imap(_search, periods), total=len(periods))
+            ):
                 Dj = new_search_data.Ds[j]
                 pass
                 snr[p], params[p] = float(self.snr(Ti, Dj, P)), (Ti, Dj, P)
@@ -201,7 +221,6 @@ class Nuance:
         if mask is None:
             mask = mask = np.ones_like(self.time).astype(bool)
 
-        masked_x = self.time[mask]
         masked_y = self.flux[mask]
         masked_X = self.X[:, mask]
 
@@ -215,7 +234,7 @@ class Nuance:
 
         return _mu()
 
-    def models(self, t0: float, D: float, P: float=None):
+    def models(self, t0: float, D: float, P: float = None):
         """Return the models corresponding the transit of epoch `t0` and duration `D`(and period `P` for a periodic transit)
 
         Parameters
@@ -252,7 +271,7 @@ class Nuance:
         m = utils.transit(self.time, t0, D, 1, P=P)
         return self._models(m)
 
-    def solve(self, t0:float, D:float, P:float=None):
+    def solve(self, t0: float, D: float, P: float = None):
         """solve linear model (design matrix `Nuance.X`)
 
         Parameters
@@ -273,7 +292,7 @@ class Nuance:
         _, w, v = self.eval_m(m)
         return w, v
 
-    def depth(self, t0: float, D: float, P: float=None):
+    def depth(self, t0: float, D: float, P: float = None):
         """depth linearly solved for epoch `t0` and duration `D` (and period `P` for a periodic transit)
 
         Parameters
@@ -293,7 +312,7 @@ class Nuance:
         w, v = self.solve(t0, D, P)
         return w[-1], np.sqrt(v[-1, -1])
 
-    def snr(self, t0: float, D: float, P: float=None):
+    def snr(self, t0: float, D: float, P: float = None):
         """SNR of transit linearly solved for epoch `t0` and duration `D` (and period `P` for a periodic transit)
 
         Parameters
@@ -361,22 +380,22 @@ class Nuance:
 
         return optimize, mu, nll
 
-    def mask(self, t0, D, P):
-        """Return a `Nuance` where the transit of epoch `t0` and duration `D` and period `P` is masked.
+    def mask(self, t0: float, D: float, P: float):
+        """Return a `Nuance` object where the transit of epoch `t0` and duration `D` and period `P` is masked.
 
         Parameters
         ----------
-        t0 : _type_
-            _description_
-        D : _type_
-            _description_
-        P : _type_
-            _description_
+        t0 : float
+            transit epoch
+        D : float
+            transit duration
+        P : float
+            transit period
 
         Returns
         -------
-        _type_
-            _description_
+        `Nuance`
+            A `Nuance` instance with transit masked.
         """
         # search data
         search_data = self.search_data.copy()
@@ -409,24 +428,54 @@ class Nuance:
         nu.search_data = search_data
         return nu
 
-    def flares_mask(self, window = 30, sigma=4, iterations=3):
+    # The all thing needs to be rewritten
+    def flares_mask(self, window=30, sigma=4, iterations=3):
+        """Return a mask where flares are masked.
+
+        Parameters
+        ----------
+        window : int, optional
+            The sliding mask window (typical number of points of a flare duration),
+            by default 30
+        sigma : int, optional
+            Flare sigma-clipping factor, by default 4
+        iterations : int, optional
+            Number of iterations, by default 3
+
+        Returns
+        -------
+        np.ndarray
+            Flare mask
+        """
+        raise NotImplementedError
         mask = np.ones_like(self.time).astype(bool)
         window = 30
 
         def build_gp(params, x):
-            return GaussianProcess(self.kernel, x, diag=np.mean(self.error)**2, mean=0.)
+            return GaussianProcess(
+                self.kernel, x, diag=np.mean(self.error) ** 2, mean=0.0
+            )
 
-        optimize, mu, nll = self.gp_optimization(build_gp)
-        
-        for i in range(iterations):
+        _, mu, _ = self.gp_optimization(build_gp)
+
+        for _ in range(iterations):
             m = mu(None)
-            r = (self.flux - m)
-            mask_up = r < np.std(r[mask])*sigma
+            r = self.flux - m
+            mask_up = r < np.std(r[mask]) * sigma
 
             # mask around flares
             ups = np.flatnonzero(~mask_up)
             if len(ups) > 0:
-                mask[np.hstack([np.arange(max(u-window, 0), min(u+window, len(self.time))) for u in ups])] = False
+                mask[
+                    np.hstack(
+                        [
+                            np.arange(
+                                max(u - window, 0), min(u + window, len(self.time))
+                            )
+                            for u in ups
+                        ]
+                    )
+                ] = False
 
             _, mu, _ = self.gp_optimization(build_gp, mask=mask)
 
@@ -441,6 +490,7 @@ class Nuance:
     @classmethod
     def load(cls, filename):
         return cls(**pickle.load(open(filename, "rb")))
+
 
 def _search(p):
     phase, _, P2 = SEARCH(p)
--- tests/test.py
+++ tests/test.py
@@ -7,13 +7,12 @@ np.random.seed(42)
 time = np.linspace(0, 6, 500)
 diff_error = 0.001
 X = np.vander(time, N=4, increasing=True)
-w = [1., 0.05, -0.2, -0.5]
-true_transit = 0.01 * utils.periodic_transit(time, 0.2/4, 0.05, P=1.3)
-diff_flux = true_transit + np.random.normal(0., diff_error, size=len(time))
+w = [1.0, 0.05, -0.2, -0.5]
+true_transit = 0.01 * utils.periodic_transit(time, 0.2 / 4, 0.05, P=1.3)
+diff_flux = true_transit + np.random.normal(0.0, diff_error, size=len(time))
 
 
 class TestTransitSearch(unittest.TestCase):
-
     def __init__(self, *args, **kwargs):
         unittest.TestCase.__init__(self, *args, **kwargs)
 
@@ -21,8 +20,10 @@ class TestTransitSearch(unittest.TestCase):
         from nuance import Nuance
         from tinygp import kernels, GaussianProcess
 
-        gp = GaussianProcess(kernels.quasisep.Exp(1e15), time, diag=diff_error**2, mean=1.)
-        nu = Nuance(time, diff_flux, gp, X.T)
+        gp = GaussianProcess(
+            kernels.quasisep.Exp(1e15), time, diag=diff_error**2, mean=1.0
+        )
+        nu = Nuance(time, diff_flux, gp=gp, X=X.T)
 
         t0s = time.copy()
         Ds = np.linspace(0.01, 0.1, 10)
@@ -30,6 +31,7 @@ class TestTransitSearch(unittest.TestCase):
 
         periods = np.linspace(1, 5, 5000)
         _ = nu.periodic_search(periods)
+
 
 if __name__ == "__main__":
     unittest.main()
