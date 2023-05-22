443ec1a0eb7fbd9e451f0f11720e85d5d7fe60fe
lgrcia <lionel_garcia@live.fr>
2023-05-22 12:03:39 +0200

    chore: black formatting

--- nuance/__init__.py
+++ nuance/__init__.py
@@ -2,8 +2,9 @@ import os
 from multiprocessing import cpu_count
 
 CPU_counts = cpu_count()
-os.environ['XLA_FLAGS'] = f"--xla_force_host_platform_device_count={CPU_counts}"
+os.environ["XLA_FLAGS"] = f"--xla_force_host_platform_device_count={CPU_counts}"
 from jax.config import config
+
 config.update("jax_enable_x64", True)
 
 from .nuance import Nuance
