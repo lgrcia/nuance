49f2eb64ef2e9ded6ff52a89f02188c131c73edf
lgrcia <lionel_garcia@live.fr>
2024-02-12 11:49:13 -0500

    chore: core non-jit + new Nuance __repr__ + docstrings

--- nuance/core.py
+++ nuance/core.py
@@ -22,13 +22,11 @@ def eval_model(flux, X, gp):
     return function
 
 
-@jax.jit
 def transit_protopapas(t, t0, D, P=1e15, c=12):
     _t = P * jnp.sin(jnp.pi * (t - t0) / P) / (jnp.pi * D)
     return -0.5 * jnp.tanh(c * (_t + 1 / 2)) + 0.5 * jnp.tanh(c * (_t - 1 / 2))
 
 
-@jax.jit
 def transit_box(time, t0, D, P=1e15):
     return -((jnp.abs(time - t0) % P) < D / 2).astype(float)
 
